799e5279cb9aed84eaf7fcad45d56c2e925f3252
tehffuhnee <41089388+tehffuhnee@users.noreply.github.com>
2018-07-31 09:10:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+// header
+function goHome() {
+  window.location.href = "home.html";
+}
+
+function goQuiz() {
+  window.location.href = "quiz.html";
+}
+
+function goGet() {
+  window.location.href = "gettingStarted.html";
+}
+
+function goWell() {
+  window.location.href = "wellness.html";
+}
+
+function goCreators() {
+  window.location.href = "creators.html";
+}
+
+// joyce routine
+function goBanila() {
+  window.location.href = "https://www.rileyrose.com/us/shop/catalog/product/RR/BEAUTY_skincare_cleaners_face_wash/1000245001";
+}
+
+function goMorning() {
+  window.location.href = "http://cosrx.kr/product_015.html"
+}
+
+function goSon() {
+  window.location.href = "https://www.sephora.com/product/beauty-water-P409633?skuId=1833466&icid2=lp_sonandpark_beauty_water_carousel:p409633"
+}
+
+function goBha() {
+  window.location.href = "http://cosrx.kr/product_025.html"
+}
+
+function goCicapair() {
+  window.location.href = "https://www.sephora.com/product/cicapair-tiger-grass-cream-P411539?skuId=1855691&icid2=products%20grid:p411539"
+}
+
+function goShiseido() {
+  window.location.href = "https://www.amazon.com/Shiseido-Anessa-Perfect-Sunscreen-60ml/dp/B0052IG7G6"
+}
+
+function goLaneige() {
+  window.location.href = "https://www.sephora.com/product/lip-sleeping-mask-P420652"
+}
+
+// tiff routine
+function goRice() {
+  window.location.href = "https://theskinfood.us/products/rice-mask-wash-off";
+}
+
+function goFoam() {
+  window.location.href = "https://us.innisfree.com/hydrating-cleansing-foam-with-green-tea/270670039.html?cgid=sc-cleanser#start=1"
+}
+
+function goSon() {
+  window.location.href = "https://www.sephora.com/product/beauty-water-P409633?skuId=1833466&icid2=lp_sonandpark_beauty_water_carousel:p409633"
+}
+
+function goCentella() {
+  window.location.href = "http://cosrx.kr/product_020.html"
+}
+
+function goSeed() {
+  window.location.href = "https://us.innisfree.com/intensive-hydrating-serum-with-green-tea-seed/270670859.html"
+}
+
+function goCica() {
+  window.location.href = "https://en.koreadepart.com/item/1489038871/too-cool-for-school-mild-cica-sun-lotion-spf50pa"
+}
+
+function goCanola() {
+  window.location.href = "https://us.innisfree.com/soft-lip-balm-with-canola-honey-deep-moisture/270670541.html?cgid=sc-lip-care#start=3"
+}
+
+// clodagh routine
+function goBlack() {
+  window.location.href = "https://www.amazon.com/Make-Clean-Black-Cleansing-6-09oz/dp/B075JS2PCQ/ref=as_sl_pc_qf_sp_asin_til?tag=joankeem-20&linkCode=w00&linkId=e884f4b63defdbf8c314c8d05d91e417&creativeASIN=B075JS2PCQ";
+}
+
+function goEnature() {
+  window.location.href = "https://sokoglam.com/products/enature-moringa-cleansing-balm?utm_source=pepperjam&utm_medium=referral&utm_campaign=affiliate&affiliateID=73861&ClickID=2388690813"
+}
+
+function goBlue() {
+  window.location.href = "https://www.glowrecipe.com/pages/glow-recipe-blueberry-bounce-gentle-cleanser"
+}
+
+function goNeogen() {
+  window.location.href = "https://www.amazon.com/Neogen-Bio-Peel-Gauze-Peeling-Lemon/dp/B019RULW50/ref=as_sl_pc_qf_sp_asin_til?tag=joankeem-20&linkCode=w00&linkId=3f7742fc8a31232b20f9d22e38fac1bd&creativeASIN=B019RULW50&th=1"
+}
+
+function goTroi() {
+  window.location.href = "https://www.amazon.com/Troiareuke-ACSEN-Pore-Control-Mask/dp/B06XF9F11Y/ref=sr_1_3_a_it?ie=UTF8&qid=1532623965&sr=8-3&keywords=troiareuke+ACSEN+Pore+Control+Mask"
+}
+
+function goLicorice() {
+  window.location.href = "https://sokoglam.com/products/acwell-licorice-ph-balancing-cleansing-toner?utm_source=pepperjam&utm_medium=referral&utm_campaign=affiliate&affiliateID=73861&ClickID=2388691147"
+}
+
+function goFerment() {
+  window.location.href = "https://sokoglam.com/products/neogen-real-ferment-micro-essence?utm_source=pepperjam&utm_medium=referral&utm_campaign=affiliate&affiliateID=73861&ClickID=2388691159"
+}
+
+function goYeux() {
+  window.location.href = "https://www.lancome-usa.com/skin-care/eye-care-and-lip-care/eye-creams/genifique-yeux/1000369.html?utm_medium=affiliate_automated&utm_campaign=QFGLnEolOWg&utm_source=linkshare&utm_content=rewardStyle&clickID=.MjUyNzQzLTA.54fadca1-90f4-11e8-bba8-4b10f1291a4e&pubID=QFGLnEolOWg&ranMID=36178&ranEAID=QFGLnEolOWg&ranSiteID=QFGLnEolOWg-YB5wEGLMcuJNKgcH1jECQg"
+}
+
+function goSafe() {
+  window.location.href = "https://www.glowrecipe.com/products/make-prem-safe-relief-cream"
+}
+
+function goLightning() {
+  window.location.href = "https://sokoglam.com/products/cosrx-triple-c-lightning-liquid?utm_source=pepperjam&utm_medium=referral&utm_campaign=affiliate&affiliateID=73861&ClickID=2388691190"
+}
